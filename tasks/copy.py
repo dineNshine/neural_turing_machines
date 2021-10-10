9ade61eb0ccf6a0b2fa9686eb922c7f890409759
dineNshine <pfre00@gmail.com>
2021-10-10 22:53:53 +0200

    exp config improved

--- tasks/copy.py
+++ tasks/copy.py
@@ -14,14 +14,14 @@ class CopyTask(Task):
 
     def get_batch(self, batch_size: int, device: torch.device) -> tuple[Tensor, Tensor, Tensor]:
         sequence_length = np.random.randint(low=self.min_sequence_length, high=self.max_sequence_length)
+        
         vector_values = np.random.randint(low=1, high=2 ** self.vector_size, size=(sequence_length * batch_size))
         binary_reprs = [np.binary_repr(vector_value, width=self.vector_size) for vector_value in vector_values]
         vectors = np.array([[int(x) for x in vector] for vector in binary_reprs], dtype=bool)
         vectors = vectors.reshape((sequence_length, batch_size, self.vector_size))
         sequence = torch.tensor(vectors, device=device)
-        delimiter = torch.zeros((1, batch_size, self.vector_size), dtype=torch.bool, device=device)
         zeros = torch.zeros((1, batch_size, self.vector_size), dtype=torch.bool, device=device)
-        inputs = torch.cat([sequence, delimiter, zeros.expand((sequence_length - 1, -1, -1))], dim=0)
+        inputs = torch.cat([sequence, zeros.expand((sequence_length, -1, -1))], dim=0)
 
         targets = torch.cat([zeros.expand(sequence_length, -1, -1), sequence], dim=0)
 
